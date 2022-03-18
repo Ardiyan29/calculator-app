0e1c6487c2044ac10ca5fe84ea41ac9a43bbe975
Ardiyan29 <ardiyanrizkinugraha@students.amikom.ac.id>
2022-03-18 20:59:34 +0700

    inisialisasi project

--- calculator.cs
+++ calculator.cs
@@ -11,5 +11,16 @@ namespace Calculator
         static void Main(string[] args)
         {
         }
+
+        static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
     }
 }
